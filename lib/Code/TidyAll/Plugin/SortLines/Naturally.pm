107760eae220dbd8615d59810573bcc7bc298d91
Olaf Alders <olaf@wundersolutions.com>
2014-07-12 20:15:29 -0400

    Initial commit

---
+++ lib/Code/TidyAll/Plugin/SortLines/Naturally.pm
@@ -0,0 +1,44 @@
+use strict;
+use warnings;
+
+package Code::TidyAll::Plugin::SortLines::Naturally;
+
+use Moo;
+extends 'Code::TidyAll::Plugin';
+
+use Sort::Naturally qw( nsort );
+
+sub transform_source {
+    my ( $self, $source ) = @_;
+
+    return join( "\n", nsort( grep {/\S/} split( /\n/, $source ) ) ) . "\n";
+}
+
+1;
+__END__
+
+=pod
+
+=head1 NAME
+
+Code::TidyAll::Plugin::SortLines::Naturally - sort lines of a file using Sort::Naturally
+
+=head1 SYNOPSIS
+
+   # In configuration:
+
+   [SortLines::Naturally]
+   select = .ispell* **/.gitignore
+
+=head1 DESCRIPTION
+
+Sorts the lines of a file; whitespace lines are discarded. Useful for files
+containing one entry per line, such as C<.svnignore>, C<.gitignore>, and
+C<.ispell*>.
+
+=head1 ACKNOWLEDGEMENTS
+
+This code was essentially pilfered from L<Code::TidyAll::Plugin::SortLines>
+
+
+1;
